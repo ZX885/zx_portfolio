09849af539b2fbc76e66eb71b70e54c45ea72b1a
ZX885 <zokirovzarruh@gmail.com>
2024-07-07 22:10:05 +0500

    last commit 07.07

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import './style/main.css'
 
-import Header from './components/header';
-import Footer from './components/footer';
-import './App.css';
+import Navbar from './components/nav/Navbar'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home.js';
+import Projects from './pages/Projects';
+import Contacts from './pages/Contact';
 
-function App(){
+function App() {
   return (
     <div className='wrapper'>
-      <Header />
-      <Footer />
+    <Navbar />
+    
+    {/* <Contacts/> */}
+    {/* <Home /> */}
+    <Projects />
+    
+    <Footer />
+      
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,12 @@
+const Project = () => {
+    return (
+        <li className="project">
+            <a href="./project-page.html">
+                <img src={project01} alt="Project img" className="project__img" />
+                <h3 className="project__title">Gaming streaming portal</h3>
+            </a>
+        </li>
+    );
+}
+
+export default Project;
